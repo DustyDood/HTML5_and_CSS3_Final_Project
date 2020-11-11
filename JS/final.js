f00aaa738eb3856d65bca0a9e1a9f827bb425628
Dusty Zoland <DikaiosuneZ@gmail.com>
2020-11-10 17:00:05 -0800

    Add in Slideshow

---
+++ JS/final.js
@@ -0,0 +1,39 @@
+/*This opens up the contact form*/
+function openSesame() {
+    document.getElementById("contactform").style.display="block";   
+}
+
+/*This closes the contact form*/
+function closeSesame() {
+    document.getElementById("contactform").style.display="none";  
+}
+
+/*This closes the contact form if open, or opens it if closed*/
+function bothWaysSesame() {
+    if(document.getElementById("contactform").style.display==="block") {
+        document.getElementById("contactform").style.display="none"; 
+    } else {
+        document.getElementById("contactform").style.display="block"
+    }
+}
+
+/*Ideally, the form would actually submit to my e-mail
+In this case, it'll just give a message that the server isn't set up yet*/
+function validationStation() {
+    var name = document.forms["formform"]["Name"].value;
+    var email = document.forms["formform"]["Email"].value;
+    var message = document.forms["formform"]["Message"].value;
+    console.log(name, email, message);
+
+    if (name == "") {
+        alert("Please enter your name in the 'Name' field!")
+    } else if (email == "") {
+        alert("Please enter your e-mail in the 'E-mail' field!")
+    } else if (message == "") {
+        alert("Please enter your message in the 'Message' field!")
+    }
+    else {
+        alert("Thank you for your submission! Unfortunately, this is still a work in progress, so no messages will be submitted. Sorry!")
+    }
+    return false;
+}
